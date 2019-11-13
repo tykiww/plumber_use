6a9c23957fc788687ea70834ecb643bcc28ab59f
Tyki Wada <36966666+tykiww@users.noreply.github.com>
2019-11-13 12:43:27 -0700

    Update create_model.R

--- poisson_mlr/create_model.R
+++ poisson_mlr/create_model.R
@@ -14,13 +14,12 @@ set.seed(15)
 idd <- sample(nrow(dta),.8*nrow(dta), replace = FALSE)
 train <- dta[idd,]
 tests <- dta[-idd,]
-glimpse(dta)
+
 
 hist(dta$Absenteeism.time.in.hours) # Looks poisson to me.
 
 # Create Model to Prepare for poisson regression
 ab_lm <- glm(Absenteeism.time.in.hours ~., data = train, family = "poisson")
-summary(ab_lm)
 
 # Save Model
 saveRDS(ab_lm, "absent_poisson.rds")
@@ -28,8 +27,6 @@ saveRDS(ab_lm, "absent_poisson.rds")
 # Load Model
 mods <- readRDS("absent_poisson.rds")
 
-# Predict using Loaded File
-pred <- predict(mods, tests)
-
-
-# https://shirinsplayground.netlify.com/2018/01/plumber/
+# Save test data as JSON
+tests_json <- jsonlite::toJSON(tests)
+save(tests_json, file="test.json")
